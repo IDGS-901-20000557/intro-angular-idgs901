0236f1a9032a759dd1834b4b3257dbba638859b4
Claustro616 <claudioracoon@hotmail.com>
2023-06-21 17:48:48 -0600

    CETT 21/06/2023

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,14 +9,7 @@ import { MenuComponent } from './grupos/menu/menu.component';
 import { FormsModule } from '@angular/forms';
 import { SumaComponent } from './grupos/formularios/suma/suma.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { OperasBasComponent } from './grupos/formularios/operas-bas/operas-bas.component';
-
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {MatRadioModule} from '@angular/material/radio';
-import {MatFormFieldModule} from '@angular/material/form-field';
-
+import { OperasModule } from './grupos/formularios/operas/operas.module';
 
 @NgModule({
   declarations: [
@@ -25,18 +18,13 @@ import {MatFormFieldModule} from '@angular/material/form-field';
     evnComponent,
     IricComponent,
     MenuComponent,
-    SumaComponent,
-    OperasBasComponent
+    SumaComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
-    MatInputModule,
-    MatButtonModule,
-    MatIconModule,
-    MatRadioModule,
-    MatFormFieldModule
+    OperasModule
   ],
   providers: [],
   bootstrap: [AppComponent]
